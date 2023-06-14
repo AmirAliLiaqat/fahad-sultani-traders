4f6e1ff923411030986b596889e010fb86b2d83b
Amir Ali <amirliaqat2020@gmail.com>
2023-06-14 09:32:31 +0500

    Updating the bill book format...

--- page-bill.php
+++ page-bill.php
@@ -32,18 +32,20 @@
             
             <h1 class="text-center text-capitalize my-5"><?php esc_html_e(the_title()); ?></h1>
 
-            <div class="bill bg-white rounded p-4 mx-auto" style="width: 60%">
-                <div class="row bill_header_section_1 mb-3">
-                    <div class="col-lg-3 col-md-3 col-sm-12">
-                        <span class="border-bottom border-dark">&nbsp;<?php echo esc_html($date); ?>&nbsp;</span>
-                        <h4 class="d-inline-block">تاریخ</h4>
-                    </div><!-- .col-lg-3 -->
-                    <div class="col-lg-9 col-md-9 col-sm-12">
-                        <h1 class="text-danger text-end">فہد سلطانی ٹریڈرز</h1>
-                    </div><!-- .col-lg-9 -->
+            <div class="bill bg-white rounded mx-auto" style="width: 60%">
+                <div class="bill_header_section_1 p-4">
+                    <div class="row fw-bolder  mb-3">
+                        <div class="col-lg-4 col-md-4 col-sm-12 ">
+                            <span class="border-bottom border-dark"><?php echo esc_html($date); ?></span>
+                            <h1 class="d-inline-block text-dark">تاریخ</h1>
+                        </div><!-- .col-lg-4 -->
+                        <div class="col-lg-8 col-md-8 col-sm-12">
+                            <h1 class="text-dark text-end display-4">فہد سلطانی ٹریڈرز</h1>
+                        </div><!-- .col-lg-8 -->
+                    </div><!-- .row -->
                 </div><!-- .bill_header_section_1 -->
 
-                <div class="row bill_header_section_2">
+                <div class="row bill_header_section_2 pt-4 px-4">
                     <div class="col-lg-3 col-md-3 col-sm-12">
                         <div class="text-center text-danger border border-dark rounded py-2">
                             حاجی محمد یونس  <br>
@@ -60,7 +62,7 @@
                     </div><!-- .col-lg-9 -->
                 </div><!-- .bill_header_section_2 -->
 
-                <div class="bill_header_section_3 d-flex justify-content-between my-2">
+                <div class="bill_header_section_3 d-flex justify-content-between my-2 px-4">
                     <div class="bill_field">
                         <span class="border-bottom border-dark">data</span>
                         <h4 class="d-inline-block">نام بیوپاری</h4>
@@ -71,7 +73,7 @@
                     </div><!-- . bill_field -->
                 </div><!-- .bill_header_section_3 -->
 
-                <div class="bill_detail">
+                <div class="bill_detail p-4">
                     <table class="table table-bordered border-dark text-center mb-0">
                         <thead>
                             <tr>
@@ -79,6 +81,7 @@
                                 <th width="100">نرخ</th>
                                 <th>تفصیل</th>
                                 <th width="30">مقدار</th>
+                                <th width="70">نمبر شمار</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -88,6 +91,7 @@
                                 <td class="p-3" width="100"></td>
                                 <td class="p-3"></td>
                                 <td class="p-3" width="30"></td>
+                                <td class="p-3" width="70"></td>
                             </tr>
                             <?php endfor; ?>
                         </tbody>
@@ -131,14 +135,13 @@
                                         <h2 class="d-inline-block">کل رقم</h4>
                                     </div><!-- . bill_field -->
                                 </td>
-                                <td class="p-3" width="45"></td>
                             </tr>
                         </tbody>
                     </table>
                 </div><!-- .bill_detail -->
 
-                <div class="bill_footer">
-                    <h3 class="bg-primary text-white text-center rounded w-100 py-2">دکان نمبر 46 نئی سبزی منڈی کاہنہ کچا فیروز پور روڈ لاہور</h3>
+                <div class="bill_footer p-3">
+                    <h3 class="text-dark text-center fw-bolder w-100 py-2">دکان نمبر 46 نئی سبزی منڈی کاہنہ کچا فیروز پور روڈ لاہور</h3>
                 </div><!-- .bill_footer -->
             </div><!-- .bill -->
 
